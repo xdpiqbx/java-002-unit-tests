23e4d451189d47759d95b20cb3e9b03600d819bb
xdpiqbx <xdpiqbx@gmail.com>
2022-11-12 23:27:04 +0200

    refactored

--- app/src/main/java/dpiqb/math/ExpressionParser.java
+++ app/src/main/java/dpiqb/math/ExpressionParser.java
@@ -1,62 +1,34 @@
 package dpiqb.math;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 public class ExpressionParser {
     public Expression parse(String expression) {
         try{
-            ParseState state = ParseState.a;
-
-            StringBuilder a = new StringBuilder();
-            StringBuilder b = new StringBuilder();
-            Expression.Operator operator = null;
+            boolean searchA = true;
 
             char[] chars = expression.toCharArray();
-            for (int i = 0; i < chars.length; i++) {
+            for (int i = 1; i < chars.length; i++) {
                 char c = chars[i];
-                if (i==0){
-                    a.append(c);
-                }else{
-                    switch (state){
-                        case a:
-                            if (isOperator(c)){
-                                operator = getOperator(c);
-                                state = ParseState.b;
-                            }else{
-                                a.append(c);
-                            }
-                            break;
-                        case b:
-                            b.append(c);
-                            break;
-                    }
+                Optional<Expression.Operator> operator = getOperator(c);
+                if(operator.isPresent()){
+                    return Expression.builder()
+                            .a(Integer.parseInt(expression.substring(0, i)))
+                            .b(Integer.parseInt(expression.substring(i + 1)))
+                            .operator(operator.get())
+                            .build();
                 }
             }
-
-            return Expression.builder()
-                    .a(Integer.parseInt(a.toString()))
-                    .b(Integer.parseInt(b.toString()))
-                    .operator(operator)
-                    .build();
         }catch (Exception ex){
             throw new IllegalArgumentException("Invalid expression: " + expression);
         }
+        throw new IllegalArgumentException("Invalid expression: " + expression);
     }
 
-    private enum ParseState{
-        a, b
-    }
-
-    private boolean isOperator(char c){
-        return Arrays.stream(Expression.Operator.values())
-                .mapToInt(it -> it.toChar())
-                .anyMatch(it -> c == it);
-    }
-
-    private Expression.Operator getOperator(char c){
+    private Optional<Expression.Operator> getOperator(char c){
         return Arrays.stream(Expression.Operator.values())
                 .filter(it -> it.toChar() == c)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException());
+                .findFirst();
     }
 }
